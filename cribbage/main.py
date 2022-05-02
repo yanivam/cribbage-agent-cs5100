6fab55348861b0e7cf60aa103675720abb5d9d9e
ding25y <44220103+ding25y@users.noreply.github.com>
2022-05-02 12:19:13 -0400

    Revert "Revert "Implemented TreeAgent with minimax""
    This reverts commit e52fe790995e98c5a452f21bb1b2cd926dfab9c9.

--- cribbage/main.py
+++ cribbage/main.py
@@ -1,11 +1,16 @@
 from argparse import ArgumentParser
-
-from .game import Game
-from .players import (
+import sys
+import os
+# SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+# # print(SCRIPT_DIR)
+# sys.path.append(os.path.dirname(SCRIPT_DIR))
+from cribbage.game import Game
+from cribbage.players import (
     WinGame,
     HumanPlayer,
     NondeterministicAIPlayer,
     EnumerativeAIPlayer,
+    TreeAIPlayer,
 )
 
 
@@ -16,6 +21,7 @@ def main():
         "human": HumanPlayer,
         "expert": EnumerativeAIPlayer,
         "random": NondeterministicAIPlayer,
+        "tree":TreeAIPlayer,
     }
 
     parser = ArgumentParser()
